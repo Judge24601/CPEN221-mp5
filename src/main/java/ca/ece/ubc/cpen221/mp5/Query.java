756ec08104a0ad5d504588e1b65c4d0af90b19e8
Judge24601 <braedenjury@gmail.com>
2017-12-01 21:37:56 -0800

    rep & specs for Query

--- src/main/java/ca/ece/ubc/cpen221/mp5/Query.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Query.java
@@ -4,6 +4,21 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * 
+ * Represents a Query to the database. Can hold instances of itself (And Expression/ Or Expression)
+ * Rep Invariant:
+ * Either exp and search for cannot be null,
+ * Or children cannot be null.
+ * FoundSet must only contain Businesses in YelpDB.
+ * Abstraction Function:
+ * Represents a parsed query from ANTLR as an object with either a String to 
+ * query the database with, or a List of other queries to compare.
+ * If it is a List & orFlag is true - represents an OrExpression
+ * If it is a List & orFlag is false -represents an AndExpression.
+ *
+ */
+
 public class Query {
 	//List<List<Query>> or;
 	List<Query> children;
@@ -11,13 +26,21 @@ public class Query {
 	String searchFor;
 	Set<Business> foundSet;
 	boolean orFlag = false;
-	//base case
+	/**
+	 * Constructs a basic query.
+	 * 
+	 * @param exp - Expression - the type of query
+	 * @param searchFor - what to query the database for
+	 */
 	public Query(String exp, String searchFor) {
 		this.exp = exp; 
 		this.searchFor = searchFor;
 		this.foundSet = new HashSet<Business>();
 	}
-	
+	/**
+	 * Constructs an Or or And Expression
+	 * @param children - andExps or atoms, respectively.
+	 */
 	public Query(List<Query> children) {
 		this.children = children;
 	}
@@ -30,7 +53,13 @@ public class Query {
 	}
 	
 	
-	//ONLY Eval OrExps
+	/**
+	 * Evaluates orExpressions by taking the intersection of all their
+	 * andExpression children, and the andExpressions taking the union of all 
+	 * their atom children. Since atom can be an orExpression, works recursively to
+	 * end up with only simple queries.
+	 * @return Set of Businesses matching the query
+	 */
 	public Set<Business> eval() {
 		Set<Business> results = new HashSet<Business>();
 		for(Query andExp: this.children) {
